c0f98f3775cda73d5e3157559661ee64a7744da8
pulperi <pulperi@gmail.com>
2020-05-08 16:54:36 +0300

    uibuild

---
+++ build/precache-manifest.c0200bc2ef29ea971227023cdd1fa86b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2ef4a3f70822fa145d305d40b0f8cfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b44385b22fbdaa8b2e7e",
+    "url": "/static/js/2.3ed13054.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3ed13054.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dda4123d036a20754b2",
+    "url": "/static/js/main.26f59d22.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
